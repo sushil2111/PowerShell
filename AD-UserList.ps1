0503cf8c1f03a69f007c21dd535cec39cfd10b72
sushil2111 <sushil.barai2111@gmail.com>
2024-10-04 13:18:09 -0700

    Update 002

--- AD-UserList.ps1
+++ AD-UserList.ps1
@@ -1,3 +1,3 @@
 $users = Get-ADuser -filter * -properties * | select name, samaccountname | export-csv $ENV:workspace\Userdetails.csv
 write-host "list of users below" 
-write-host "$users"
+write-host "$($users)"
