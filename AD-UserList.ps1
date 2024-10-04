0788a699b4c0bd2c2c79c6e81ad34568964d420a
sushil2111 <sushil.barai2111@gmail.com>
2024-10-04 13:22:42 -0700

    004

--- AD-UserList.ps1
+++ AD-UserList.ps1
@@ -1,3 +1,5 @@
-$users = Get-ADuser -filter * -properties * | select name, samaccountname | export-csv $ENV:workspace\Userdetails.csv
+$users = Get-ADuser -filter * -properties * | select name, samaccountname
+$userdetails = Get-ADuser -filter * -properties * | select name, samaccountname | export-csv $ENV:workspace\Userdetails.csv
+
 write-host "list of users below" 
 write-host $users
